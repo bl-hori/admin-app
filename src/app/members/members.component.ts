41b23f00bbec8603967d8e61c697fc468a85638a
Naoto Hori <naoto.hori@broadleaf.co.jp>
2022-07-14 00:01:16 +0900

    8. 社員リストを選択可能にする

--- src/app/members/members.component.ts
+++ src/app/members/members.component.ts
@@ -16,9 +16,15 @@ export class MembersComponent implements OnInit {
     name: '田中太郎',
   };
 
+  selectedMember: Member | undefined;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSelect(member: Member): void {
+    this.selectedMember = member;
   }
 
 }
